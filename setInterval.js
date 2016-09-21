558fd686564709ff06f8dc758f79b87fd4e4cbcc
Cheng Cui <cuicheng11165@gmail.com>
2016-09-21 10:59:04 +0800

    add

--- setInterval.js
+++ setInterval.js
@@ -5,10 +5,7 @@ var fuck=function(){
 var interNo1=setInterval(fuck,5000);
 
 
-var i = 0;
-var scrollAction = function(){
-	document.body.scrollTop=(i+=1000);
-}
-var scrollNo=setInterval(scrollAction,2000);
+
+
 
 
